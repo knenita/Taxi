464672ff0a9ca873ac0c62e93211bb90d90a3f68
Diana Russi <diana.russiposada@hotmail.com>
2020-03-01 15:50:46 -0500

    Add locator services

---
+++ Taxi.Common/Services/IGeolocatorService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Taxi.Common.Services
+{
+    public interface IGeolocatorService
+    {
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+        Task GetLocationAsync();
+    }
+}
--- Taxi.Prism/Taxi.Prism.Android/MainActivity.cs
+++ Taxi.Prism/Taxi.Prism.Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.CurrentActivity;
+using Plugin.Permissions;
 using Prism;
 using Prism.Ioc;
 
@@ -16,8 +18,15 @@ namespace Taxi.Prism.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            CrossCurrentActivity.Current.Init(this, bundle); // inicializar libreria geolocalizador
+            global::Xamarin.Forms.Forms.Init(this, bundle); //inicaliza tema mapas
             LoadApplication(new App(new AndroidInitializer()));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
@@ -29,4 +38,3 @@ namespace Taxi.Prism.Droid
         }
     }
 }
-
